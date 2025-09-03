eb153f4ee21c6f8260bd8fd15f1ba6b592afbac2
saying121 <saying121@outlook.com>
2025-09-03 14:30:28 +0800

    refactor!: add perf data support

--- rust/build.rs
+++ rust/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    tonic_build::compile_protos("psh.proto").unwrap();
+    tonic_build::configure()
+        .build_server(false)
+        .compile_protos(&["../proto/psh.proto"], &["../proto"])
+        .unwrap();
+    // tonic_build::compile_protos("../proto/psh.proto").unwrap();
 }

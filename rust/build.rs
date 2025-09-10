d4792885de7630e846ff318199edc98454c73f9f
saying121 <saying121@outlook.com>
2025-09-10 09:04:18 +0800

    fix: build server

--- rust/build.rs
+++ rust/build.rs
@@ -1,6 +1,5 @@
 fn main() {
     tonic_build::configure()
-        .build_server(false)
         .compile_protos(&["../proto/psh.proto"], &["../proto"])
         .unwrap();
     // tonic_build::compile_protos("../proto/psh.proto").unwrap();
